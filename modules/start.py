e8b5ead1652c00383e81da0285d4308a95c9a0fe
vaibhavchandra12 <77480184+vaibhavchandra12@users.noreply.github.com>
2021-09-10 16:54:37 +0530

    Update start.py

--- modules/start.py
+++ modules/start.py
@@ -46,18 +46,21 @@ async def start_(client: Client, message: Message):
                 [
                     InlineKeyboardButton(
                         "➕ 𝐀𝐝𝐝 𝐓𝐨 𝐘𝐨𝐮𝐫 𝐆𝐫𝐨𝐮𝐩 ➕", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
-                ],[
+                ],
+                [
                     InlineKeyboardButton(
                          "🤔𝐂𝐨𝐦𝐦𝐚𝐧𝐝𝐬", url=f"https://t.me/IncognitoNetwork/8"
                     )
-                ],[
+                ],
+                [
                     InlineKeyboardButton(
                         "💬 𝐒𝐮𝐩𝐩𝐨𝐫𝐭", url=f"https://t.me/IncognitoOff"
                     ),
                     InlineKeyboardButton(
                         "🔊 𝐔𝐩𝐝𝐚𝐭𝐞𝐬", url=f"https://t.me/IncognitoNetwork"
                     )
-                ],[
+                ],
+                [
                     InlineKeyboardButton(
                         "𝐒𝐨𝐮𝐫𝐜𝐞 𝐂𝐨𝐝𝐞💞", url=f"https://t.me/addyxd""
                     )
